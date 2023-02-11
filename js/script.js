ac1d03dc2314a5e449ff83f763fb4df0cdf29435
JhonataGust <brjhouplay@gmail.com>
2023-02-10 21:03:46 -0300

    api half done

--- js/script.js
+++ js/script.js
@@ -1,42 +1,21 @@
 function main() {
+  const HOST = 'http://127.0.0.1:3000/v1/';
+  let albumCard = null;
   const btnDialog = document.querySelector("#btnActionDialog");
   const dialogBox = document.querySelector(".formMain");
   const buttonCloseDialog = document.querySelector(".closeDialog");
   const addLyricButton = document.querySelector("#LyricButton");
   const formInputSub = document.querySelector("#formInputSub");
-
+  const music_input = document.querySelector("#music_input")
   const mainCard = document.querySelector(".cardPopularAndRecommended");
-  const albumCard = [
-    {
-      id: 0,
-      image: "https://www.vagalume.com.br/travis-scott/discografia/birds-in-the-trap-sing-mcknight.webp",
-      name: "AstroWorld",
-      description: "test test test test test test",
-      songSize: "2:30"
-  }
-];
+  let subs_arr = [] 
 
-  albumCard.forEach((el) => {
-    mainCard.insertAdjacentHTML("beforeend", `
-        <figure class="albumCard">
-          <div class="albumImageCard">
-              <span class="material-symbols-outlined">favorite</span>
-              <img src="images/imageAlbum01.webp" alt="" class="albumImage">
-          </div>
-          <div class="albumDescription">
-              <figcaption class="albumTitle">${el.name}</figcaption>
-              <p class="albumParagraph">${el.description}</p>
-          </div>
-          <div class="songSize">
-              <span class="size">${el.songSize}</span>
-              <span class="material-symbols-outlined">share</span>
-          </div>
-      </figure>
-    `);
-  }); 
+  getMusics();
+
+
 
   //Click actions
-
+  music_input.addEventListener("change", removeOrAddActions);
   btnDialog.addEventListener("click", addDialog);
   buttonCloseDialog.addEventListener("click", removeDialog);
   addLyricButton.addEventListener("click", addSubTitles);
@@ -49,11 +28,46 @@ function main() {
     dialogBox.style.display = "none";
   }
 
+  function removeOrAddActions({target}){
+    url_blob = URL.createObjectURL(target.files[0]);
+    document.getElementById('afterChange').remove();
+    document.getElementById('afterChange').insertAdjacentHTML("beforeend", `
+        <div style='margin:20px 0px'>
+        <div style='margin:20px 0px'>
+          <audio controls id="audio_preview">
+            <source src="${url_blob}">
+          </audio>
+        </div>
+        <a class="input_upload">Remove Music</a>
+        </div>
+    `);
+  }
+
+
+
   function addSubTitles() {
+    let audio_ref = document.getElementById('audio_preview');
     let list = document.createElement("p");
     list.className = "chip_list";
-    list.innerHTML = formInputSub.value + "time: 12sec";
+    subs_arr.push({time: audio_ref.currentTime, sub_text: formInputSub.value})
+    list.innerHTML = formInputSub.value + "time: " + audio_ref.currentTime;
     document.getElementById("subs").appendChild(list);
+  }
+  async function getMusics() {
+    const response = await fetch(`${HOST}/musics`)
+    albumCard = await response.json();
+    console.log(albumCard);
+  }
+
+  async function createMusic(){
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    return response.json();
   }
 }
 
