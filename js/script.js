cb8ad33fd6393010571f2d5fc279f4d21e7873f8
JhonataGust <brjhouplay@gmail.com>
2023-02-10 22:49:24 -0300

    sading music

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 function main() {
-  const HOST = 'http://127.0.0.1:3000/v1/';
-  let albumCard = null;
+  const HOST = "http://127.0.0.1:3000/v1/";
   const btnDialog = document.querySelector("#btnActionDialog");
   const dialogBox = document.querySelector(".formMain");
   const buttonCloseDialog = document.querySelector(".closeDialog");
   const addLyricButton = document.querySelector("#LyricButton");
   const formInputSub = document.querySelector("#formInputSub");
-  const music_input = document.querySelector("#music_input")
+  const music_input = document.querySelector("#music_input");
   const mainCard = document.querySelector(".cardPopularAndRecommended");
   const submitForm = document.querySelector("#submitForm");
-  let subs_arr = [] 
-
+  const form = document.querySelector("form");
+  let subs_arr = [];
+  let music_duration = 0;
   getMusics();
-
-  //Click actions
+  //Actions
   music_input.addEventListener("change", removeOrAddActions);
   btnDialog.addEventListener("click", addDialog);
   buttonCloseDialog.addEventListener("click", removeDialog);
@@ -27,11 +26,14 @@ function main() {
     dialogBox.style.display = "none";
   }
 
-  function removeOrAddActions({target}){
+  function removeOrAddActions({ target }) {
+    music_file = target.files[0];
     url_blob = URL.createObjectURL(target.files[0]);
-    submitForm.style.display = 'unset';
-    document.getElementById('afterChange').remove();
-    document.getElementById('afterChange').insertAdjacentHTML("beforeend", `
+    submitForm.style.display = "unset";
+    document.getElementById("afterChange").remove();
+    document.getElementById("afterChange").insertAdjacentHTML(
+      "beforeend",
+      `
         <div style='margin:20px 0px'>
         <div style='margin:20px 0px'>
           <audio controls id="audio_preview">
@@ -40,37 +42,62 @@ function main() {
         </div>
         <a class="input_upload">Remove Music</a>
         </div>
-    `);
+    `
+    );
   }
 
-
-
   function addSubTitles() {
-    let audio_ref = document.getElementById('audio_preview');
+    let audio_ref = document.getElementById("audio_preview");
     let list = document.createElement("p");
     list.className = "chip_list";
-    subs_arr.push({time: audio_ref.currentTime, sub_text: formInputSub.value})
+    subs_arr.push({
+      time: audio_ref.currentTime,
+      sub_text: formInputSub.value,
+    });
     list.innerHTML = formInputSub.value + "time: " + audio_ref.currentTime;
     document.getElementById("subs").appendChild(list);
   }
   async function getMusics() {
-    const response = await fetch(`${HOST}/musics`)
-    albumCard = await response.json();
-    console.log(albumCard);
+    const response = await fetch(`${HOST}musics`);
+    let albumCard = await response.json();
+
+    console.log(albumCard['user_musics']);
+
+    albumCard['user_musics'].forEach((el) => {
+      mainCard.insertAdjacentHTML("beforeend", `
+          <figure class="albumCard">
+            <div class="albumImageCard">
+                <span class="material-symbols-outlined">favorite</span>
+                <img src="images/imageAlbum01.webp" alt="" class="albumImage">
+            </div>
+            <div class="albumDescription">
+                <figcaption class="albumTitle">${el.name}</figcaption>
+                <p class="albumParagraph">${el.description}</p>
+            </div>
+            <div class="songSize">
+                <span class="size">${el.song_size}</span>
+                <span class="material-symbols-outlined">share</span>
+            </div>
+        </figure>
+      `);
+    }); 
   }
 
-  async function createMusic(){
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    return response.json();
-  }
+  form.addEventListener("submit", (event) => {
+    let audio_ref = document.getElementById("audio_preview");
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append('song_size', audio_ref.duration);
+    formData.append('subs_payload',JSON.stringify(subs_arr));
+    fetch(`${HOST}/musics`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  });
 }
 
 main();
-
-
